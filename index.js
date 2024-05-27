66e977374094be481b23bdb075568325a6d54029
Abdulloh Ahmadaliyev <abdulloh.dev.843@gmail.com>
2024-05-27 12:06:39 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,6 @@
+let backround = prompt("orqa fonni kriting:");
+let color = prompt("oldi  foni");
+
+let body = document.querySelector("body")
+
+body.style = `background-color: ${backround}; color: ${color}`
